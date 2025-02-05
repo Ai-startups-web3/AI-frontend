bc2102e171ce593791e028befa4cc9a9d31ea932
shubhamkunwar10 <fonders@metakul.com>
2025-02-05 09:43:36 +0530

    update url

--- src/Datatypes/enums.ts
+++ src/Datatypes/enums.ts
@@ -1,6 +1,6 @@
 
 // const base_url_backend="https://backend-everything-37ada44e5086.herokuapp.com/v1"
-const base_url_backend="http://localhost:5000/backend/v1"
+const base_url_backend="https://ai-prompt-generator-iwbe.onrender.com/backend/v1"
 
 // define endpoints here
   export const ApiEndpoint: Record<string, any> = {
